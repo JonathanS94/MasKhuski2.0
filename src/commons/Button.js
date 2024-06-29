b240c2873c956e844f34e5c57a1a0bc5e02fcb5b
JonathanS94 <jonathan5445@live.com>
2024-06-28 22:20:38 -0500

    fear:[MSK-09][Jonathan]Implentacion Control de ingreso de datos

--- src/commons/Button.js
+++ src/commons/Button.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Button as StrapButton } from "reactstrap";
 
-const Button = ({ color, onClick, value, href, className }) => {
+const Button = ({ color, onClick, value, href, className, disabled }) => {
   return (
     <StrapButton
       color={color}
       onClick={onClick}
       href={href}
       className={className}
+      disabled={disabled}
     >
       {value}
     </StrapButton>
--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // reactstrap components
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Progress } from "reactstrap";
 import Image from "commons/Image.js";
 import Button from "commons/Button";
 import Input from "commons/Input";
 //Estilo
 import { useStyles } from "./PagerHeader.style";
+
 export default function PageHeader() {
   const classes = useStyles();
+  //Componentes Usuario
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [progress, setProgress] = useState(0);
+  // Validacion de campos vacios y 
+  useEffect(() => {
+    let completedFields = 0;
+    if (name.length > 0) completedFields++;
+    if (age.length > 0) completedFields++;
+    setProgress((completedFields / 2) * 100);
+
+    if (completedFields === 2) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [name, age]);
+  // Validacion de solo ingrese hasta 20 letras
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    if (/^[a-zA-Z\s]*$/.test(value) && value.length <= 20) {
+      setName(value);
+    }
+  };
+  // Validacion de solo ingrese hasta dos digitos
+  const handleAgeChange = (e) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value) && value.length <= 2) {
+      setAge(value);
+    }
+  };
   return (
     <div className="page-header header-filter">
       <div className="squares square1" />
@@ -40,13 +73,26 @@ export default function PageHeader() {
             <Input
               className={classes.input}
               placeholder="Ingrese su Nombre o Alias "
+              value={name}
+              onChange={handleNameChange}
             />
-            <Input className={classes.input} placeholder="Ingrese su Edad " />
+            <Input
+              className={classes.input}
+              placeholder="Ingrese su Edad "
+              value={age}
+              onChange={handleAgeChange}
+            />
+            <Progress
+              className={classes.progress}
+              value={progress}
+              color="success"
+            />
             <Button
               className={classes.button}
               color="success"
               value="JUGAR"
               href="/niveles"
+              disabled={isButtonDisabled}
             />
           </Col>
         </Row>
